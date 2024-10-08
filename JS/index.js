c450480edfdf883fe2abcbf95f1a029684c0f06b
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2024-10-08 13:27:31 -0300

    Inseriindo a logica e conexao para retornar da api as informacoes de nome e imagem dos pokemons, alem de criar uma funcao que recebe a quantidade de pokemons que queremos capturar

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,13 @@
-//CRIANDO A REQUISIÇÃO
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+//CHAMANDO A FUNÇÃO E DEFINIR A QUANTIDADE DE POKEMON QUEREMOS
+
+let quantidadePokemon = document.getElementById('quantidade')
+quantidadePokemon.addEventListener('keyup', ()=>{
+    capturarPokemon(parseInt(quantidadePokemon.value))
+})
+
+function capturarPokemon(quantidade){
+    //CRIANDO A REQUISIÇÃO
+fetch('https://pokeapi.co/api/v2/pokemon?limit=' + quantidade)
 .then(response => response.json())
 .then(allPokemon =>{
 
@@ -16,14 +24,24 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
             nome: value.name,
             imagem: pokemon.sprites.front_default// recuperando as imagens
             })
-            
 
-            if(pokemons.length === 10){
-                console.log('ok');
+            if(pokemons.length === quantidade){      
+            
+                var pokemonBoxes = document.querySelector('.pokemon-boxes')
+                pokemonBoxes.innerHTML = ""
+            
+                pokemons.map((valores)=>{
+                    pokemonBoxes.innerHTML += 
+                    `
+                        <div class="pokemon-box">
+                            <img src="${valores.imagem}" alt="Um pokemon">
+                            <p>${valores.nome}</p>
+                        </div>
                 
+                    `
+                })
             }
         })
-
     })
-
 })
+}
